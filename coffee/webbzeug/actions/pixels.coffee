ff75608a6490876cb80d56e95b220f712f42a9c4
Sascha Gehlich <sascha@gehlich.us>
2012-10-07 01:37:13 +0200

    pixels action now has a seeeeed

--- coffee/webbzeug/actions/pixels.coffee
+++ coffee/webbzeug/actions/pixels.coffee
@@ -1,6 +1,37 @@
+`var CustomRandom = function(nseed) {
+
+    var seed,
+        constant = Math.pow(2, 13)+1,
+        prime = 37,
+        maximum = Math.pow(2, 50);
+ 
+    if (nseed) {
+        seed = nseed;
+    }
+ 
+    if (seed == null) {
+        seed = (new Date()).getTime();
+    } 
+ 
+    return {
+        next : function() {
+            seed *= constant;
+            seed += prime;
+            seed %= maximum;
+            
+            return seed;
+        }
+    }
+}`
+
 window.Webbzeug ?= {}
 window.Webbzeug.Actions ?= {}
 window.Webbzeug.Actions.Pixels = class PixelsAction extends Webbzeug.Action
+  availableParameters: ->
+    {
+      seed: { name: 'Seed', type: 'number', min: 0, max: 999999999, default: Math.round(Math.random() * 999999999) },
+    }
+
   render: (contexts) ->
     super()
 
@@ -16,8 +47,9 @@ window.Webbzeug.Actions.Pixels = class PixelsAction extends Webbzeug.Action
 
     # @context.putImageData imageData, 0, 0 
 
+    custRnd = CustomRandom(@getParameter('seed'))
     for i in [0...imageData.data.length / 4]
-      rand = Math.random()
+      rand = custRnd.next() / Math.pow(2, 50)
       rand = rand * 255
       index = (i << 2)
       imageData.data[index] = rand
--- js/webbzeug/actions/pixels.js
+++ js/webbzeug/actions/pixels.js
@@ -1,4 +1,30 @@
 (function() {
+  var CustomRandom = function(nseed) {
+
+    var seed,
+        constant = Math.pow(2, 13)+1,
+        prime = 37,
+        maximum = Math.pow(2, 50);
+ 
+    if (nseed) {
+        seed = nseed;
+    }
+ 
+    if (seed == null) {
+        seed = (new Date()).getTime();
+    } 
+ 
+    return {
+        next : function() {
+            seed *= constant;
+            seed += prime;
+            seed %= maximum;
+            
+            return seed;
+        }
+    }
+};
+
   var PixelsAction, _base, _ref, _ref1,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -19,12 +45,25 @@
       return PixelsAction.__super__.constructor.apply(this, arguments);
     }
 
+    PixelsAction.prototype.availableParameters = function() {
+      return {
+        seed: {
+          name: 'Seed',
+          type: 'number',
+          min: 0,
+          max: 999999999,
+          "default": Math.round(Math.random() * 999999999)
+        }
+      };
+    };
+
     PixelsAction.prototype.render = function(contexts) {
-      var i, imageData, index, rand, _i, _ref2;
+      var custRnd, i, imageData, index, rand, _i, _ref2;
       PixelsAction.__super__.render.call(this);
       imageData = this.context.getImageData(0, 0, this.app.getWidth(), this.app.getHeight());
+      custRnd = CustomRandom(this.getParameter('seed'));
       for (i = _i = 0, _ref2 = imageData.data.length / 4; 0 <= _ref2 ? _i < _ref2 : _i > _ref2; i = 0 <= _ref2 ? ++_i : --_i) {
-        rand = Math.random();
+        rand = custRnd.next() / Math.pow(2, 50);
         rand = rand * 255;
         index = i << 2;
         imageData.data[index] = rand;
