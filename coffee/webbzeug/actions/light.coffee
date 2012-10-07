f5cb0cc0c6e28c0c4a9c9aa2c54399c2788499ad
Sascha Gehlich <sascha@gehlich.us>
2012-10-07 12:07:56 +0200

    light

--- coffee/webbzeug/actions/light.coffee
+++ coffee/webbzeug/actions/light.coffee
@@ -1,3 +1,62 @@
 window.Webbzeug ?= {}
 window.Webbzeug.Actions ?= {}
 window.Webbzeug.Actions.Light = class LightAction extends Webbzeug.Action
+  availableParameters: ->
+    {
+      x: { name: 'X', type: 'number', min: 0, max: 256, default: 32 },
+      y: { name: 'Y', type: 'number', min: 0, max: 256, default: 128 }
+    }
+
+  render: (contexts) ->
+    super()
+
+    @context.canvas.width *= 2
+
+    if contexts.length is 0
+      @context.fillStyle = 'black'
+      @context.fillRect 0, 0, @app.getWidth(), @app.getHeight()
+
+      imageData = @context.getImageData 0, 0, @app.getWidth(), @app.getHeight()
+      imagePixelData = imageData.data
+    else
+      imageData = contexts[0].getImageData 0, 0, @app.getWidth(), @app.getHeight()
+      @context.putImageData imageData, 0, 0
+      imagePixelData = imageData.data
+
+    w = @app.getWidth()
+    h = @app.getHeight()
+
+    posX = @getParameter('x')
+    posY = @getParameter('y')
+
+    deltaHypotenuse = []
+    for x in [w...0]
+      for y in [h...0]
+        V = (x - w / 2) / 128
+        W = (y - h / 2) / 128
+
+        deltaHypotenuse[y * w + x] = (Math.max(0, 1 - Math.sqrt(V * V + W * W)) * 256)
+
+    for x in [w...0]
+      for y in [h...0]
+
+        px1 = imagePixelData[@getPixelIndex(x, y + 1) + 2]
+        px2 = imagePixelData[@getPixelIndex(x, y - 1) + 2]
+        px3 = imagePixelData[@getPixelIndex(x + 1, y) + 2]
+        px4 = imagePixelData[@getPixelIndex(x - 1, y) + 2]
+
+        intensity = @luminosity(deltaHypotenuse[@luminosity(px1 - px2 - y + posY) * w + @luminosity(px3 - px4 - x + posX)])
+
+        for i in [3...0]
+          imagePixelData[@getPixelIndex(x, y) + i] = intensity
+
+        imagePixelData[@getPixelIndex(x, y) + 3] = w - 1
+
+    @context.putImageData imageData, 0, 0
+    return @context
+
+  getPixelIndex: (x, y) ->
+    return (y * 256 + x) * 4
+
+  luminosity: (d) ->
+    return Math.round(Math.min(Math.max(d, 0), @app.getWidth() - 1))
--- js/webbzeug/actions/light.js
+++ js/webbzeug/actions/light.js
@@ -19,6 +19,76 @@
       return LightAction.__super__.constructor.apply(this, arguments);
     }
 
+    LightAction.prototype.availableParameters = function() {
+      return {
+        x: {
+          name: 'X',
+          type: 'number',
+          min: 0,
+          max: 256,
+          "default": 32
+        },
+        y: {
+          name: 'Y',
+          type: 'number',
+          min: 0,
+          max: 256,
+          "default": 128
+        }
+      };
+    };
+
+    LightAction.prototype.render = function(contexts) {
+      var V, W, deltaHypotenuse, h, i, imageData, imagePixelData, intensity, posX, posY, px1, px2, px3, px4, w, x, y, _i, _j, _k, _l, _m;
+      LightAction.__super__.render.call(this);
+      this.context.canvas.width *= 2;
+      if (contexts.length === 0) {
+        this.context.fillStyle = 'black';
+        this.context.fillRect(0, 0, this.app.getWidth(), this.app.getHeight());
+        imageData = this.context.getImageData(0, 0, this.app.getWidth(), this.app.getHeight());
+        imagePixelData = imageData.data;
+      } else {
+        imageData = contexts[0].getImageData(0, 0, this.app.getWidth(), this.app.getHeight());
+        this.context.putImageData(imageData, 0, 0);
+        imagePixelData = imageData.data;
+      }
+      w = this.app.getWidth();
+      h = this.app.getHeight();
+      posX = this.getParameter('x');
+      posY = this.getParameter('y');
+      deltaHypotenuse = [];
+      for (x = _i = w; w <= 0 ? _i < 0 : _i > 0; x = w <= 0 ? ++_i : --_i) {
+        for (y = _j = h; h <= 0 ? _j < 0 : _j > 0; y = h <= 0 ? ++_j : --_j) {
+          V = (x - w / 2) / 128;
+          W = (y - h / 2) / 128;
+          deltaHypotenuse[y * w + x] = Math.max(0, 1 - Math.sqrt(V * V + W * W)) * 256;
+        }
+      }
+      for (x = _k = w; w <= 0 ? _k < 0 : _k > 0; x = w <= 0 ? ++_k : --_k) {
+        for (y = _l = h; h <= 0 ? _l < 0 : _l > 0; y = h <= 0 ? ++_l : --_l) {
+          px1 = imagePixelData[this.getPixelIndex(x, y + 1) + 2];
+          px2 = imagePixelData[this.getPixelIndex(x, y - 1) + 2];
+          px3 = imagePixelData[this.getPixelIndex(x + 1, y) + 2];
+          px4 = imagePixelData[this.getPixelIndex(x - 1, y) + 2];
+          intensity = this.luminosity(deltaHypotenuse[this.luminosity(px1 - px2 - y + posY) * w + this.luminosity(px3 - px4 - x + posX)]);
+          for (i = _m = 3; _m > 0; i = --_m) {
+            imagePixelData[this.getPixelIndex(x, y) + i] = intensity;
+          }
+          imagePixelData[this.getPixelIndex(x, y) + 3] = w - 1;
+        }
+      }
+      this.context.putImageData(imageData, 0, 0);
+      return this.context;
+    };
+
+    LightAction.prototype.getPixelIndex = function(x, y) {
+      return (y * 256 + x) * 4;
+    };
+
+    LightAction.prototype.luminosity = function(d) {
+      return Math.round(Math.min(Math.max(d, 0), this.app.getWidth() - 1));
+    };
+
     return LightAction;
 
   })(Webbzeug.Action);
