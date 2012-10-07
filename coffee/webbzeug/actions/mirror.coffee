cef23ecb620716192fb0c827e866eba7c066fadf
Sascha Gehlich <sascha@gehlich.us>
2012-10-07 02:01:44 +0200

    mirror

--- coffee/webbzeug/actions/mirror.coffee
+++ coffee/webbzeug/actions/mirror.coffee
@@ -1,3 +1,24 @@
 window.Webbzeug ?= {}
 window.Webbzeug.Actions ?= {}
 window.Webbzeug.Actions.Mirror = class MirrorAction extends Webbzeug.Action
+  render: (contexts) ->
+    super()
+    if contexts.length == 0
+      console.log "Dude a mirror needs an input"
+      return
+
+    # How to copy the image data from one context to another
+    imageData = contexts[0].getImageData 0, 0, @app.getWidth(), @app.getHeight()
+   
+    for y in [0...imageData.height]
+      ySrcOffset  = (y * imageData.width) << 2
+      yDrawOffset = (y * imageData.width + imageData.width - 1) << 2
+
+      for x in [0...imageData.width / 2]
+        imageData.data[yDrawOffset - (x << 2)] = imageData.data[ySrcOffset + (x << 2)]
+        imageData.data[yDrawOffset - (x << 2) + 1] = imageData.data[ySrcOffset + (x << 2) + 1]
+        imageData.data[yDrawOffset - (x << 2) + 2] = imageData.data[ySrcOffset + (x << 2) + 2]
+        imageData.data[yDrawOffset - (x << 2) + 3] = imageData.data[ySrcOffset + (x << 2) + 3]
+
+    @context.putImageData imageData, 0, 0 
+    return @context
--- js/webbzeug/actions/mirror.js
+++ js/webbzeug/actions/mirror.js
@@ -19,6 +19,28 @@
       return MirrorAction.__super__.constructor.apply(this, arguments);
     }
 
+    MirrorAction.prototype.render = function(contexts) {
+      var imageData, x, y, yDrawOffset, ySrcOffset, _i, _j, _ref2, _ref3;
+      MirrorAction.__super__.render.call(this);
+      if (contexts.length === 0) {
+        console.log("Dude a mirror needs an input");
+        return;
+      }
+      imageData = contexts[0].getImageData(0, 0, this.app.getWidth(), this.app.getHeight());
+      for (y = _i = 0, _ref2 = imageData.height; 0 <= _ref2 ? _i < _ref2 : _i > _ref2; y = 0 <= _ref2 ? ++_i : --_i) {
+        ySrcOffset = (y * imageData.width) << 2;
+        yDrawOffset = (y * imageData.width + imageData.width - 1) << 2;
+        for (x = _j = 0, _ref3 = imageData.width / 2; 0 <= _ref3 ? _j < _ref3 : _j > _ref3; x = 0 <= _ref3 ? ++_j : --_j) {
+          imageData.data[yDrawOffset - (x << 2)] = imageData.data[ySrcOffset + (x << 2)];
+          imageData.data[yDrawOffset - (x << 2) + 1] = imageData.data[ySrcOffset + (x << 2) + 1];
+          imageData.data[yDrawOffset - (x << 2) + 2] = imageData.data[ySrcOffset + (x << 2) + 2];
+          imageData.data[yDrawOffset - (x << 2) + 3] = imageData.data[ySrcOffset + (x << 2) + 3];
+        }
+      }
+      this.context.putImageData(imageData, 0, 0);
+      return this.context;
+    };
+
     return MirrorAction;
 
   })(Webbzeug.Action);
