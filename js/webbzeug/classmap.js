3b2d6a9d83e9ce2fb892084f7117f0e265089428
Daniel Hoelzgen <daniel.hoelzgen@me.com>
2013-09-24 18:05:39 +0200

    Added compiled classmap

--- js/webbzeug/classmap.js
+++ js/webbzeug/classmap.js
@@ -33,11 +33,6 @@
       type: 'generative',
       "class": Webbzeug.Actions.Fractal
     },
-    perlin: {
-      name: 'Perlin',
-      type: 'generative',
-      "class": Webbzeug.Actions.Perlin
-    },
     pixels: {
       name: 'Pixels',
       type: 'generative',
